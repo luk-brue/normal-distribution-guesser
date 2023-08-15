1ca2827f0a6adf43e5481bda0fbb59517d3b1393
Lukas Bruelheide <zur@posteo.de>
2023-08-15 17:49:35 +0200

    add comments

--- app.R
+++ app.R
@@ -8,12 +8,11 @@ verteilungen <- c("Normal", "Uniform", "Bimodal")
 # Define UI
 ui <- fluidPage(
 
+  # initialisiere Zusatz-Pakete
   shinyFeedback::useShinyFeedback(),
   shinyjs::useShinyjs(),
 
-    # Application title
     titlePanel("Verteilungen erraten"),
-
 
     sidebarLayout(
         sidebarPanel(
@@ -35,70 +34,86 @@ ui <- fluidPage(
     )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic
 server <- function(input, output, session) {
-sampler <- function() sample(verteilungen, size = 1)
-zufall <- reactiveValues(sample = sampler())
-counter <- reactiveValues(richtig = 0, falsch = 0, round = 0)
 
-observeEvent(input$button, counter$round <- counter$round + 1)
+  # Zufällig eine Verteilung aussuchen
+  sampler <- function() sample(verteilungen, size = 1)
+  # reaktiv machen
+  zufall <- reactiveValues(sample = sampler())
 
-observeEvent(input$n, {
-  feedback(inputId = "n", text = "unmöglich", color = "darkred",
-           show = {input$n < 30})
-  feedback(inputId = "n", text = "schwer", color = "red",
-           show = {input$n > 30 && input$n <= 39})
-  feedback(inputId = "n", text = "mittelschwer", color = "orange",
-           show = {input$n > 39 && input$n <= 50})
-  feedback(inputId = "n", text = "medium", color = "yellow",
-                  show = {input$n > 50 && input$n <= 100})
-  feedback(inputId = "n", text = "leicht", color = "darkgreen",
-                  show = {input$n > 100 && input$n <= 200})
-  feedback(inputId = "n", text = "sehr leicht", color = "lightgreen",
-           show = {input$n > 200})
+  # initialisiere reaktive Variablen für verschiedene Zähler
+  counter <- reactiveValues(richtig = 0, falsch = 0, round = 0)
 
-})
+  # Runden-Zähler
+  observeEvent(input$button, counter$round <- counter$round + 1)
 
-observeEvent(input$reset, {
-  zufall$sample <- sampler()
-  counter$richtig <- 0
-  counter$falsch <- 0
-  counter$round <- 0
-})
+  # Momentan nur teilweise funktionierende Logik um
+  # Schwierigkeitsgrad farbig darzustellen
+  observeEvent(input$n, {
 
+      feedback(inputId = "n", text = "unmöglich", color = "darkred",
+               show = {input$n < 30})
+      feedback(inputId = "n", text = "schwer", color = "red",
+               show = {input$n > 30 && input$n <= 39})
+      feedback(inputId = "n", text = "mittelschwer", color = "orange",
+               show = {input$n > 39 && input$n <= 50})
+      feedback(inputId = "n", text = "medium", color = "yellow",
+                      show = {input$n > 50 && input$n <= 100})
+      feedback(inputId = "n", text = "leicht", color = "darkgreen",
+                      show = {input$n > 100 && input$n <= 200})
+      feedback(inputId = "n", text = "sehr leicht", color = "lightgreen",
+               show = {input$n > 200})
+
+  })
+
+  # Reset-Button
+  observeEvent(input$reset, {
+    zufall$sample <- sampler()
+    counter$richtig <- 0
+    counter$falsch <- 0
+    counter$round <- 0
+  })
+
+  # Erfolgsquote
     output$text <- renderText({
-
             paste("Erfolgsquote:",
+                  # Prüfe den Rundenzähler
                   if(counter$round >= 5){
                     paste(
                       round(
                         counter$richtig / (counter$richtig + counter$falsch) * 100,
-                        digits = 2
-                        ),
+                        digits = 2),
                       "%")
-                    } else {
-                      "wird erst ab 5 Versuchen angezeigt"
-                    }
-          )
-
+                    } else {"wird erst ab 5 Versuchen angezeigt"})
       })
 
+    # Histogramm
     output$plot <- renderPlot({
-      input$button
+
+      input$button # bewirkt dass der Plot neu gerendert wird durch Knopf
+
+      # Mögliche Verteilungen definieren und eine auswählen
       x <- switch(zufall$sample,
              "Normal" = rnorm(input$n),
              "Uniform" = runif(input$n),
-             "Bimodal" = c(rnorm(input$n / 2), (rnorm(input$n / 2) + 5))
-      )
-      df <- data.frame(x)
+             "Bimodal" = c(rnorm(input$n / 2), (rnorm(input$n / 2) + 5)))
+
+      # Sturges-Regel aber etwas angepasst, weil es zu wenig Bins für meinen Geschmack waren
       bins <- as.integer(nclass.Sturges(x) * 1.4)
+
+      # Plot
+      df <- data.frame(x)
       ggplot(df, aes(x = x)) +
         geom_histogram(bins = bins, fill = "gray", color = "black") +
         theme_minimal()
     })
-    observeEvent(input$radio,
-                 {
+
+    # Radio Buttons
+    observeEvent(input$radio, {
+                   # erfordere Auswahl bevor es weitergeht
                    req(input$radio)
+                   # Bewertung des Inputs
                    test <- input$radio == zufall$sample
                    if(test){
                      counter$richtig <- counter$richtig + 1
@@ -110,16 +125,26 @@ observeEvent(input$reset, {
                      showToast(type = "error", message = zufall$sample,
                                keepVisible = TRUE)
                    }
+                   # Verhindere weitere Auswahl durch Deaktivieren
                    disable("radio")
                  })
-    observeEvent(input$button,
-                 {updateRadioButtons(session, inputId = "radio",
+
+    # Nächster Plot-Knopf
+    observeEvent(input$button, {
+
+      # 1. Auswahlknöpfe auf "unausgewählt" zurücksetzen
+      updateRadioButtons(session, inputId = "radio",
                                     selected = character(0))
-                   zufall$sample <- sampler() # Zufallsgenerator
+
+                   # 2. zufällig eine der drei Verteilungen auswählen
+                   zufall$sample <- sampler()
+
+                   # 3. Reaktiviere Auswahlknöpfe
                    enable("radio")
                    hideToast(animate = F)
                  })
 
+    # Tabelle mit richtig und falsch
     output$table <- renderTable({
       data.frame(richtig = counter$richtig, falsch = counter$falsch)
       },
